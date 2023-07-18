53a7029de45a1855e437929b7582c8085eb7519d
moon5381 <moon5381@naver.com>
2023-07-18 17:08:21 +0900

    FEAT:검색기능완료

--- src/redux/login-info/login.slice.ts
+++ src/redux/login-info/login.slice.ts
@@ -1,18 +1,31 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import axios from 'axios';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {API_ROUTE} from '@env';
 const initialState: LiteState = {
 	login: [],
 	userInfo: [],
+	a: '',
 };
 
-export const postUserLogin = createAsyncThunk('POST/Login', async (data, thunkAPI) => {
+const axiosAuth = axios.create({
+	baseURL: API_ROUTE,
+	headers: {'content-type': 'application/json'},
+});
+
+export const socialLogin = createAsyncThunk('/auth/login', async (data: LoginType, thunkAPI) => {
 	try {
-		const response = await axios.post('http://3.39.14.168:8080/auth/login', data);
-		console.log('성공', response.data);
-		return thunkAPI.fulfillWithValue(response.data);
+		const response = await axiosAuth.post(
+			'/auth/login',
+			{userName: data.userName, userProfileImage: data.userProfileImage, userToken: data.userToken},
+			{
+				headers: {
+					Authorization: 'Bearer ' + data.userToken,
+				},
+			},
+		);
+		return response.data;
 	} catch (error) {
-		console.log('실패', error);
 		return thunkAPI.rejectWithValue(error);
 	}
 });
@@ -25,7 +38,29 @@ export const loginSlice = createSlice({
 			state.login = payload;
 		},
 	},
+	extraReducers: builder => {
+		builder.addCase(socialLogin.fulfilled, (state, {payload}) => {
+			setStorage('token', payload.userJwtToken);
+			state.a = payload.userJwtToken;
+		});
+	},
 });
+
+// get
+export const getStorage = async (key: string) => {
+	const result = await AsyncStorage.getItem(key);
+	return result && JSON.parse(result);
+};
+
+// set
+export const setStorage = async (key: string, value: string) => {
+	return await AsyncStorage.setItem(key, JSON.stringify(value));
+};
+
+// remove
+export const removeStorage = async (key: string) => {
+	return await AsyncStorage.removeItem(key);
+};
 
 export const loginSliceActions = loginSlice.actions;
 export default loginSlice.reducer;
@@ -33,10 +68,11 @@ export default loginSlice.reducer;
 interface LiteState {
 	login: LoginType[];
 	userInfo: string[];
+	a: string;
 }
 
 interface LoginType {
-	userName: string;
-	userProfileImage: string;
-	userToken: string;
+	userName: string | null;
+	userProfileImage: string | null;
+	userToken: string | null;
 }
