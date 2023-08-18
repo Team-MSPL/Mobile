e6c692ee84ba0be33c733577ff9a2fe86ea44f83
moon5381 <moon5381@naver.com>
2023-08-18 16:34:29 +0900

    FIX:풀리퀘

--- src/screens/community/community-main-screen.tsx
+++ src/screens/community/community-main-screen.tsx
@@ -38,7 +38,6 @@ export default function CommunityMainScreen({navigation}: any) {
 	useFocusEffect(
 		useCallback(() => {
 			fetchCommunityData();
-			console.log('CommunityMainScreen 갱신됨');
 		}, []),
 	);
 
