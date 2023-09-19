d5bb87e220eb36e632f5712e41ebba750d7d4be5
luke0126 <luke0126@naver.com>
2023-09-19 21:15:00 +0900

    [FEAT] 메인 화면 디자인 적용

--- src/utill/base-modal.tsx
+++ src/utill/base-modal.tsx
@@ -1,8 +1,8 @@
+import {Modal, Text, TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
-import {TouchableOpacity, Modal, Text} from 'react-native';
 import {useAppDispatch, useAppSelector} from '../redux';
 import {modalSliceActions} from '../redux/modal/modalSlice';
-import {TitleText, SubText} from './component/policy/policy1';
+import {SubText, TitleText} from './component/policy/policy1';
 
 export default function BaseModal() {
 	const {modalOpen, modalLeft, modalTitle, modalSubTitle, modalFunction} = useAppSelector(state => state.modalSlice);
@@ -18,7 +18,7 @@ export default function BaseModal() {
 		<Modal
 			animationType={'fade'}
 			presentationStyle={'formSheet'}
-			transparent={true}
+			//transparent={true}
 			visible={modalOpen}
 			onRequestClose={close}>
 			<ModalContainer onPress={close}>
