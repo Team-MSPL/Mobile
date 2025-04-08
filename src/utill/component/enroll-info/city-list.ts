fb480c89563530d5fa59c7a518054a8fcb21775a
moon5381 <moon5381@naver.com>
2025-04-08 11:02:37 +0900

    FIX:지역매칭오타수정

--- src/utill/component/enroll-info/city-list.ts
+++ src/utill/component/enroll-info/city-list.ts
@@ -15,7 +15,7 @@ const koreaCityList = [
 			{id: 4, subId: 5, subTitle: '속초시'},
 			{id: 9, subId: 2, subTitle: '경주시'},
 			{id: 9, subId: 22, subTitle: '포항시'},
-			{id: 10, subId: 13, subTitle: '여수시'},
+			{id: 8, subId: 13, subTitle: '여수시'},
 		],
 	},
 	{
